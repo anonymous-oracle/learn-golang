d53a47b1e1e3d739ee14fa149628ff9244580a89
anonymous-oracle <suhass149@outlook.com>
2025-04-12 15:57:43 +0530

    used the short declaration operator a little

---
+++ variables-values-types/variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 5
+	fmt.Println("The value of the variable x that was declared and assigned is ", x)
+
+	x = 10
+	fmt.Println("The value of x was then reassigned to a value of ", x)
+
+	y := x * 10
+	fmt.Println("The value of x is then used in an expression where it is multiplied by 10 to give a value of ", y)
+
+	str := "When light shines the brightest, the shadows are the darkest..."
+	fmt.Println("Here's a string:\n", str)
+}
