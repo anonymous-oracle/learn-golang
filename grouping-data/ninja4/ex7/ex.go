7e9b366175806aacc284f61502140dace92bbf52
anonymous-oracle <suhass149@outlook.com>
2025-04-24 10:28:37 +0530

    finished ninja4 ex7

---
+++ grouping-data/ninja4/ex7/ex.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	r1 := []string{"James", "Bond", "Shaken, not stirred"}
+	r2 := []string{"Miss", "Moneypenny", "Hello James"}
+
+	mslice := [][]string{r1, r2}
+
+	for _, v := range mslice {
+		for _, rv := range v {
+			fmt.Println(rv)
+		}
+	}
+
+}
