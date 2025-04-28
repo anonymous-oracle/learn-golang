e4cc23f293ff7d1e3241cb0b67c9bb301a8fe285
anonymous-oracle <suhass149@outlook.com>
2025-04-28 22:56:01 +0530

    finished func expression

---
+++ functions/func-expression/fexp.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	f := func ()  {
+		fmt.Println("this is a func expression")
+	}
+
+	f()
+
+	g := func(x int)  {
+		fmt.Println("The year big brother started watching:", x)
+	}
+	g(1984)
+
+}
