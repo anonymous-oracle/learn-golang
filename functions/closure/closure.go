fe0491e4353382c919723be9be2acc04ab0aaade
anonymous-oracle <suhass149@outlook.com>
2025-04-30 22:37:26 +0530

    functions done; ready for ninja6

--- functions/closure/closure.go
+++ functions/closure/closure.go
@@ -10,4 +10,17 @@ func main()  {
 	fmt.Println(x)
 	x++
 	fmt.Println(x)
+	foo()
+	fmt.Println(x)
+
+	{ // this block ensures the closure of y's scope 
+		y := 42
+		fmt.Println(y) // y has scope limited to this code block
+	}
+	// fmt.Println(y) // y is out of scope here
+}
+
+func foo()  {
+	fmt.Println("calling from foo")
+	x++
 }
---
+++ functions/closure2/closure.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main()  {
+	a := incrementor()
+	b := incrementor()
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a())
+	fmt.Println(a()) // prints 4 since the a now has an initialised variable x with 0
+	fmt.Println(b()) // but since incrementor is called again, it is creating a new variable in memory with x in b's scope
+	fmt.Println(b())
+	fmt.Println(b())
+	fmt.Println(b())
+}
+
+func incrementor() func() int {
+	var x int
+	return func() int {
+		x++
+		return x
+	}
+}
