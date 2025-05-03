5b01084824d6b0fe6e11617b6d560896dc5fad03
anonymous-oracle <suhass149@outlook.com>
2025-05-03 22:21:40 +0530

    sorting done

--- json/sort/sort.go
+++ json/sort/sort.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
-func main()  {
-	xi := []int{4,7,3,42,99,18,16,56,12}
+func main() {
+	xi := []int{4, 7, 3, 42, 99, 18, 16, 56, 12}
 	xs := []string{"James", "Q", "M", "Moneypenny", "Dr. No"}
 
 	fmt.Println(xi)
 	fmt.Println(xs)
+
+	// sorting the slices
+
+	sort.Ints(xi)    // sorts in place
+	sort.Strings(xs) // sorts in place
+
+	fmt.Println(xi)
+	fmt.Println(xs)
+
 }
