9d63e2b9fe08c9ba733262ec0b00d713ad341f65
anonymous-oracle <suhass149@outlook.com>
2025-05-06 17:36:41 +0530

    basic concurrency by wait group setup

---
+++ concurrency/concurrency/concurrency.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main()  {
+	fmt.Println("OS\t", runtime.GOOS)
+	fmt.Println("ARCH\t", runtime.GOARCH)
+	fmt.Println("CPUs\t", runtime.NumCPU())
+	fmt.Println("Goroutines\t", runtime.NumGoroutine())
+
+	wg.Add(1) // when launching a go routine, add 1 to the wait group counter so that the wait group can wait on go routines
+
+	go foo() // runs concurrent execution; but to ensure execution add the concurrent routine calls to a wait group
+	bar()
+	fmt.Println("CPUs\t", runtime.NumCPU())
+	fmt.Println("Goroutines\t", runtime.NumGoroutine())
+	wg.Wait() // wait on the added go routines to finish because once the main() exits the execution stops
+}
+
+
+func foo()  {
+	for i := 0; i < 10; i++ {
+		fmt.Println("foo:", i)
+	}
+
+	wg.Done() // signalling the wait group to notify that the routine has successfully ended
+}
+
+func bar()  {
+	for i := 0; i < 10; i++ {
+		fmt.Println("bar:", i)
+	}
+}
